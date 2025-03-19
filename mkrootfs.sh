bc8cb513cdf9d5672a9b2eb6f2ccdfd038a0e34e
916BGAI <916772719@qq.com>
2025-03-19 14:57:58 +0800

    add the packages required by NanoKVM and PiKVM

--- mkrootfs.sh
+++ mkrootfs.sh
@@ -18,15 +18,16 @@ if [ -z "$USER_PACKAGE" ]; then
 fi
 
 BASE_PACKAGE="ca-certificates locales dosfstools binutils file \
-	tree sudo bash-completion memtester openssh-server wireless-regdb \
-	wpasupplicant systemd-timesyncd usbutils parted systemd-sysv \
-	iperf3 stress-ng avahi-daemon tmux screen i2c-tools net-tools \
-	ethtool ckermit lrzsz minicom picocom btop neofetch iotop htop \
-	bmon e2fsprogs nvi tcpdump alsa-utils squashfs-tools evtest \
-	pssh tcl-expect tcl atftp udpcast u-boot-menu initramfs-tools \
-	bluez bluez-hcidump bluez-tools btscanner bluez-alsa-utils \
-	device-tree-compiler debian-archive-keyring linux-cpupower \
-    network-manager exfatprogs cloud-guest-utils xfsprogs"
+    tree sudo bash-completion memtester openssh-server wireless-regdb \
+    wpasupplicant systemd-timesyncd usbutils parted systemd-sysv \
+    iperf3 stress-ng avahi-daemon tmux screen i2c-tools net-tools \
+    ethtool ckermit lrzsz minicom picocom btop neofetch iotop htop \
+    bmon e2fsprogs nvi tcpdump alsa-utils squashfs-tools evtest \
+    pssh tcl-expect tcl atftp udpcast u-boot-menu initramfs-tools \
+    bluez bluez-hcidump bluez-tools btscanner bluez-alsa-utils \
+    device-tree-compiler debian-archive-keyring linux-cpupower \
+    network-manager exfatprogs cloud-guest-utils xfsprogs rsync neovim \
+    xz-utils curl"
 
 if [ -z "$DESKTOP_PACKAGE" ]; then
     DESKTOP_PACKAGE="chromium task-xfce-desktop \
@@ -35,10 +36,30 @@ if [ -z "$DESKTOP_PACKAGE" ]; then
     fonts-noto-ui-core tango-icon-theme"
 fi
 
-NOGUI=0
-if [ "${NOGUI}" -eq 1 ]; then
-    DESKTOP_PACKAGE=""
+if [ -z "$KVM_PACKAGE" ]; then
+    KVM_PACKAGE="nginx bc expect v4l-utils iptables nfs-common dialog \
+    iptables dnsmasq git tesseract-ocr tesseract-ocr-eng libasound2-dev \
+    libsndfile-dev libspeexdsp-dev libdrm-dev libdbus-1-dev libglib2.0-dev \
+    libsystemd-dev libevent-pthreads-2.1-7 libgssdp-1.6-0 libgupnp-1.6-0 \
+    libgupnp-igd-1.0-4 liblua5.3-0 libmicrohttpd12 libnanomsg5 libnice10 \
+    libpaho-mqtt1.3 librabbitmq4 libsofia-sip-ua0 libsrtp2-1 libusrsctp2 \
+    libwebsockets17 libwebsockets-dev libjansson4 libogg0 libopus0 libssl3 \
+    libsystemd0 zlib1g libconfig9 libcurl4 libduktape207 libxkbcommon0 \
+    xkb-data libevent-2.1-7 libx264-164 libx264-dev libyuv-dev libyuv0 \
+    isc-dhcp-server"
 fi
+
+case "$1" in
+    "kvm")
+        DESKTOP_PACKAGE=""
+        ;;
+    "nogui")
+        DESKTOP_PACKAGE=""
+        ;&
+    *)
+        KVM_PACKAGE=""
+        ;;
+esac
 
 mkdir build
 
@@ -71,7 +92,12 @@ deb ${MIRROR}/debian/ ${CODENAME}-updates main contrib non-free non-free-firmwar
         --customize-hook='chroot "$1" systemctl disable avahi-daemon.service' \
         --customize-hook='cp overlay/etc/systemd/system/prekvm.service "$1/etc/systemd/system/"' \
         --customize-hook='chroot "$1" systemctl enable prekvm' \
-        --include="${BASE_PACKAGE} ${DESKTOP_PACKAGE} ${USER_PACKAGE}" >./build/rootfs.tar
+        --customize-hook='chroot "$1" mkdir -p --mode=0755 /usr/share/keyrings' \
+        --customize-hook='chroot "$1" sh -c "curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.noarmor.gpg | tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null"' \
+        --customize-hook='chroot "$1" sh -c "curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list"' \
+        --customize-hook='chroot "$1" apt update' \
+        --customize-hook='chroot "$1" sh -c "apt install -y tailscale=1.80.3"' \
+        --include="${BASE_PACKAGE} ${DESKTOP_PACKAGE} ${KVM_PACKAGE} ${USER_PACKAGE}" >./build/rootfs.tar
 }
 
 genrootfs
